c37ec8508ab636a01aa0ac439db9fcfe0f9cfcd9
Davi Azarias <davi.cabral@sou.unifal-mg.edu.br>
2026-05-19 10:37:58 -0300

    Caracteres não alfabéticos agora aparecem no abstract.

--- src/main/java/com/elasticsearch/search/service/SearchService.java
+++ src/main/java/com/elasticsearch/search/service/SearchService.java
@@ -43,7 +43,7 @@ public class SearchService {
 
     private String treatContent(String content) {
         content = content.replaceAll("</?(som|math)\\d*>", "");
-        content = content.replaceAll("[^A-Za-z\\s]+", "");
+//        content = content.replaceAll("[^A-Za-z\\s]+", "");
         content = content.replaceAll("\\s+", " ");
         content = content.replaceAll("^\\s+", "");
         return content;
